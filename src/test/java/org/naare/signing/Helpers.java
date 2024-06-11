0391d41da731d649e1cd2182ee4068d3f9aa352b
Heiti Tobi <heiti.tobi@nortal.com>
2024-06-12 00:05:58 +0300

    DD4J-1059 Update helpers

--- src/test/java/org/naare/signing/Helpers.java
+++ src/test/java/org/naare/signing/Helpers.java
@@ -6,8 +6,10 @@ import org.digidoc4j.ContainerBuilder;
 import org.digidoc4j.DataToSign;
 import org.digidoc4j.SignatureBuilder;
 import org.digidoc4j.SignatureProfile;
+import org.digidoc4j.SignatureToken;
 import org.digidoc4j.signers.PKCS11SignatureToken;
 import org.digidoc4j.signers.PKCS12SignatureToken;
+import org.joda.time.DateTime;
 
 public class Helpers {
 
@@ -27,22 +29,25 @@ public class Helpers {
                 .build();
     }
 
-    public static PKCS11SignatureToken getDefaultPkcs11SignatureToken (String PinCode) {
+    public static PKCS11SignatureToken getDefaultPkcs11SignatureToken(String PinCode) {
         return new PKCS11SignatureToken("C:\\Program Files\\IDEMIA\\AWP\\DLLs\\OcsCryptoki.dll", PinCode.toCharArray(), 1);
     }
 
-    public static PKCS12SignatureToken getDefaultPkcs12SignatureToken (String PinCode) {
+    public static PKCS12SignatureToken getDefaultPkcs12SignatureToken(String PinCode) {
         return new PKCS12SignatureToken("src\\test\\resources\\keystores\\sign_ECC_from_TEST_of_ESTEID2018.p12", PinCode.toCharArray());
     }
 
-    public static DataToSign getDataToSign(Container container, PKCS11SignatureToken token, SignatureProfile profile) {
-        return SignatureBuilder.aSignature(container).
-                withSigningCertificate(token.getCertificate()).withSignatureProfile(profile).buildDataToSign();
+    public static DataToSign getDataToSign(Container container, SignatureToken token, SignatureProfile profile) {
+        return SignatureBuilder
+                .aSignature(container)
+                .withSigningCertificate(token.getCertificate())
+                .withSignatureProfile(profile)
+                .buildDataToSign();
     }
 
-    public static DataToSign getDataToSign(Container container, PKCS12SignatureToken token, SignatureProfile profile) {
-        return SignatureBuilder.aSignature(container).
-                withSigningCertificate(token.getCertificate()).withSignatureProfile(profile).buildDataToSign();
+    public static String getDateTime() {
+        DateTime time = new DateTime();
+        return time.toString("dMMy_") + time.getMillisOfDay();
     }
 
 }
