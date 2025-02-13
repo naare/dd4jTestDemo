65d8adce9aca119763919686d6c56a5c12fcfef2
Heiti Tobi <heiti.tobi@nortal.com>
2025-02-13 14:12:08 +0000

    DD4J-1159 Add example for validating container on exact date

--- src/test/java/org/naare/SignatureValidationExamplesTest.java
+++ src/test/java/org/naare/SignatureValidationExamplesTest.java
@@ -1,6 +1,5 @@
 package org.naare;
 
-import org.apache.commons.io.FileUtils;
 import org.digidoc4j.Configuration;
 import org.digidoc4j.Container;
 import org.digidoc4j.ContainerBuilder;
@@ -8,16 +7,17 @@ import org.digidoc4j.ContainerOpener;
 import org.digidoc4j.ContainerValidationResult;
 import org.digidoc4j.ValidationResult;
 import org.digidoc4j.exceptions.DigiDoc4JException;
+import org.digidoc4j.impl.ValidatableContainer;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SignatureValidationTest {
+public class SignatureValidationExamplesTest {
 
     @Test
     public void validateSignatureTest() throws IOException {
@@ -55,7 +55,7 @@ public class SignatureValidationTest {
 
         Container container = ContainerBuilder.
                 aContainer().withConfiguration(configuration).
-                fromExistingFile("src/test/resources/containers/historical/TEST_Plus_SignedWithData_10062024_72713780.asice").
+                fromExistingFile("src/test/resources/containers/historical/1_ASICE_TEST.asice").
                 build();
 
         /* Reading a file to a stream */
@@ -65,6 +65,36 @@ public class SignatureValidationTest {
 //        Container container = ContainerOpener.open(inputStream, true); // With big files support enabled
 
         ValidationResult result = container.validate();
+
+        boolean isSignatureValid = result.isValid();
+
+        List<DigiDoc4JException> validationErrors = result.getErrors();
+        List<DigiDoc4JException> validationWarnings = result.getWarnings();
+        List<DigiDoc4JException> containerErrors = ((ContainerValidationResult) result).getContainerErrors();
+        List<DigiDoc4JException> containerWarnings = ((ContainerValidationResult) result).getContainerWarnings();
+
+        /* See the validation report in XML (for debugging only - DO NOT USE YOUR APPLICATION LOGIC ON IT) */
+        String validationReport = ((ContainerValidationResult) result).getReport();
+
+        /* Save DD4 report and DSS reports */
+//        container.validate().saveXmlReports(Paths.get("<path_to_save_reports>"));
+
+        assertTrue(isSignatureValid);
+    }
+
+    @Test
+    public void validateAtDateTest() {
+        Configuration configuration = Configuration.of(Configuration.Mode.TEST);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2023, Calendar.DECEMBER, 12, 13, 0, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        Date date = calendar.getTime();
+
+        /* For validateAt() one needs to cast container to ValidatableContainer */
+        ValidatableContainer validatableContainer = (ValidatableContainer) ContainerOpener.open("src/test/resources/containers/historical/1_ASICE_TEST.asice", configuration);
+
+        ValidationResult result = validatableContainer.validateAt(date);
 
         boolean isSignatureValid = result.isValid();
 
